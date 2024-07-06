f7391a2cb1cd0077aec6f745ac9c2062845893dd
Afa <afa.alone@gmail.com>
2024-07-06 14:39:28 +0300

    autoload with gem

--- boot.rb
+++ boot.rb
@@ -1,39 +1,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
+require 'zeitwerk'
 require 'sequel'
 require 'yaml'
 require_relative 'app'
 
-$LOAD_PATH << File.expand_path('../app/interactors', __FILE__)
-$LOAD_PATH << File.expand_path('../app/validators', __FILE__)
-
 # autoload classes (app/)
-def autold(path)
-  $LOAD_PATH << path if File.directory?(path)
-  files = Dir[File.join(path, '**/*.rb')].map do |f|
-    f.delete_suffix('.rb').delete_prefix(path + '/').delete_prefix(path).split('/')
-  end.sort_by(&:size)
-  flist = files.inject([]) { |ar, i| ar << i.dup }
-  classes = flist.map { |lst| lst.pop.split('_').map(&:capitalize).join }
-  mods = flist.map do |ar|
-    ar.map { |i| i.split('_').map(&:capitalize).join }
-  end
-  classes.each_with_index do |cl, idx|
-    if mods[idx].empty?
-      autoload(cl.to_sym, files[idx].join('/'))
-      next
-    end
-    m = mods[idx].inject(Object) do |rez, md|
-      unless rez.const_defined?(md)
-        rez.const_set(md, Module.new)
-      end
-      rez.const_get(md)
-    end
-    m.autoload(cl.to_sym, files[idx].join('/'))
-  end
-end
-Dir['./app/*'].each { |p| autold(p) }
+loader = Zeitwerk::Loader.new
+Dir['./app/*'].each { |p| loader.push_dir(p) }
+loader.setup
 
 begin
   App.config = YAML.load_file('config.yml', symbolize_names: true)
